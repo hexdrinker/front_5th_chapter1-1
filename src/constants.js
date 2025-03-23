e6bfb689926072cf19b6ba27d01da84d2891bc61
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-23 15:30:32 +0900

    chore: mock posts 추가

---
+++ src/constants.js
@@ -0,0 +1,36 @@
+const MOCK_POSTS = [
+  {
+    id: 1,
+    content: "오늘 날씨가 정말 좋네요. 다들 좋은 하루 보내세요!",
+    createdAt: "5분 전",
+    thumbnail:
+      "https://menu.moneys.co.kr/moneyweek/thumb/2025/01/10/06/2025011016581868696_1.jpg",
+    author: "윈터",
+  },
+  {
+    id: 2,
+    content: "아 저녁 뭐 먹지?",
+    createdAt: "15분 전",
+    thumbnail:
+      "https://img.khan.co.kr/lady/2023/07/25/news-p.v1.20230725.e1c02b0bdeb04153bc35d5f1e7c58952.png",
+    author: "공유",
+  },
+  {
+    id: 3,
+    content: "항해 화이팅",
+    createdAt: "2시간 전",
+    thumbnail:
+      "https://i.namu.wiki/i/5T0t5RmKUuagSX92QJJXRbH6alkHcUUuCIGrKvhBsw_m7F9VNIMP_jj9iOFOUmeHkZ1eRomkeyi7yhyUAd4eIg.webp",
+    author: "예지",
+  },
+  {
+    id: 4,
+    content: "오늘이 일요일이라니,,,",
+    createdAt: "4시간 전",
+    thumbnail:
+      "https://entertainimg.kbsmedia.co.kr/cms/uploads/PERSON_20220421091123_3fc3eded50f5447a91bc6a6d6631eded.jpg",
+    author: "강동원",
+  },
+];
+
+export { MOCK_POSTS };
