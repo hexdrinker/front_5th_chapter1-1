75d67687977147049194e51d3a89c9e6c39870cd
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-23 16:20:52 +0900

    feat: footer 컴포넌트 추가

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+const Footer = () => `
+  <footer class="bg-gray-200 p-4 text-center">
+    <p>&copy; ${new Date().getFullYear()} 항해플러스. All rights reserved.</p>
+  </footer>
+`;
+
+export default Footer;
