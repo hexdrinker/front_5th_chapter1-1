92aa60b4de7555e45c28b2573ef5d97d1d92d860
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-23 18:11:49 +0900

    feat: main.js, main.hash.js 파일 구현

--- src/main.hash.js
+++ src/main.hash.js
@@ -1,1 +1,24 @@
-import "./main.js";
+import HashRouter from "./HashRouter";
+import HomePage from "./pages/home";
+import ProfilePage from "./pages/profile";
+import LoginPage from "./pages/login";
+import ErrorPage from "./pages/error";
+import { keepAuth } from "./services/auth";
+
+const routes = {
+  "/": HomePage,
+  "/profile": ProfilePage,
+  "/login": LoginPage,
+  404: ErrorPage,
+};
+
+if (!document.getElementById("root")) {
+  const root = document.createElement("div");
+  root.id = "root";
+  document.body.appendChild(root);
+}
+
+const hashRouter = new HashRouter(routes);
+
+window.navigateTo = (path) => hashRouter.navigateTo(path);
+keepAuth();
